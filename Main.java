69b77baf50d9aae2147eac758fa305a5ff9438d0
DejaVu <ilkanseckin@gmail.com>
2026-01-28 15:15:52 +0300

    Added task A
    moreover added TODO reminders to help project members

---
+++ Main.java
@@ -0,0 +1,59 @@
+package CS102_HW1_TEAMWORK;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    // Initial variables
+    public static ArrayList<Integer> randNumbers = new ArrayList<Integer>();
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
+    private static int capacity;
+
+    // Main function managing the project
+    public static void main(String[] args) {
+
+        System.out.print("Please enter the capacity of the list: ");
+        capacity = input.nextInt();
+        createList();
+        printList(randNumbers); // Use only if you want to take a look at the list
+        // Also you can use this method to display every lists program have
+        
+        // TODO: Here must be for loop that shows menu options 
+        // (maybe creating new method and call it in here)
+
+        // Do not forget to close input at the end
+        input.close();
+    }
+
+    // Creates a list for the user
+    public static void createList(){
+        for(int i=0; i<capacity; i++){
+            randNumbers.add(random.nextInt(100));
+        }
+    }
+
+    // Prints the list formatted as you wish
+    public static void printList(ArrayList<Integer> list){
+        
+        for (int i=0; i<list.size(); i++){
+            System.out.printf("%-3d|", i+1);
+        }
+        System.out.printf("%n");
+        for (int i=0; i<list.size(); i++){
+            System.out.printf("%-3d|", list.get(i));
+        }
+        System.out.printf("%n");
+    }
+
+    // TODO: diplayMenu method (b)
+
+    // TODO: minList (c)
+
+    // TODO: maxList (c)
+
+    // TODO: averageList (d)
+
+    // TODO: sumList (e)
+}
